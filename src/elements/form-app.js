b8014a35a5104f40ea088d2e8e24e27c3a9fc597
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2021-08-20 12:39:56 +0200

    Versión 1.0.1: Validar y Limpiar el formulario

--- src/elements/form-app.js
+++ src/elements/form-app.js
@@ -17,20 +17,47 @@ class FormApp extends PolymerElement {
           margin-top: 1em;
         }
         paper-button {
-          background-color: var(--accent-color);
-          color: white;
           margin-right: 0px;
           margin-top: 2.5em;
           float: right;
-        }  
+        }          
+        paper-button.custom {
+          --paper-button-ink-color: var(--paper-pink-a200);
+          /* These could also be individually defined for each of the
+            specific css classes, but we'll just do it once as an example */
+          --paper-button-flat-keyboard-focus: {
+            background-color: var(--paper-pink-a200) !important;
+            color: white !important;
+          };
+          --paper-button-raised-keyboard-focus: {
+            background-color: var(--paper-pink-a200) !important;
+            color: white !important;
+          };
+        }
+        paper-button.custom:hover {
+          background-color: var(--paper-indigo-100);
+        }
+        paper-button.indigo {
+          background-color: var(--paper-indigo-500);
+          color: white;
+          --paper-button-raised-keyboard-focus: {
+            background-color: var(--paper-pink-a200) !important;
+            color: white !important;
+          };
+        }
+        paper-button:disabled,
+        paper-button[disabled] {
+          color: white;
+          background-color: bisque;
+        }        
       </style>    
 
       <iron-form id="formulario">
           <form>
-            <paper-input label="Pequeña descripción ..." id="description"></paper-input>  
-            <paper-textarea label="Copia el codigo aquí..." id="code" rows="5"></paper-textarea>
-            <paper-input label="Lenguaje..." id="language"></paper-input> 
-            <paper-button raised id="botonGuardar">Guardar snippet de código</paper-button>   
+            <paper-input label="Pequeña descripción ..." id="description" on-value-changed="_validarFormulario"  required></paper-input>  
+            <paper-textarea label="Copia el codigo aquí..." id="code" rows="5" on-value-changed="_validarFormulario" required></paper-textarea>
+            <paper-input label="Lenguaje..." id="language" on-value-changed="_validarFormulario" required></paper-input> 
+            <paper-button raised id="botonGuardar" class="custom indigo" disabled >Guardar snippet de código</paper-button>   
           </form>
       </iron-form>
     `;
@@ -40,7 +67,16 @@ class FormApp extends PolymerElement {
       collection: {
         type: Array,
         notify: true
-      }      
+      },
+      formDescripcion: {
+        type: String
+      },
+      formCode: {
+        type: String
+      },
+      formLanguage: {
+        type: String
+      }
     };
   }
   static get listeners(){
@@ -54,15 +90,43 @@ class FormApp extends PolymerElement {
     this.$.botonGuardar.addEventListener("click", () => {
       this.addSnippet();
     });
+
   }  
+  _validarFormulario(evt){
+
+    if(evt.target.id === 'description'){
+        this.formDescripcion =  evt.target.value
+    }
+
+    if(evt.target.id === 'code'){
+      this.formCode =  evt.target.value
+    }
+
+    if(evt.target.id === 'language'){
+      this.formLanguage =  evt.target.value
+    }
+
+    if(this.formDescripcion !== "" && this.formCode !== "" && this.formLanguage !== ""){
+      this.$.botonGuardar.disabled = false;
+    }else{
+      this.$.botonGuardar.disabled = true;
+    }
+  }
   addSnippet(){
+
     var codeObj = {
       description: this.$.description.value,
       code: this.$.code.value,
       language: this.$.language.value
     }
-    // Agregar el Snippet ala coleccion
+    // Agregar el Snippet a la coleccion
     this.push("collection", codeObj);
+
+    // Limpiamos el formulario
+    this.$.description.value = '';
+    this.$.code.value = '';
+    this.$.language.value = '';
+
   }
 }
 
